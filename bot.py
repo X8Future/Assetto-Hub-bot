8da5d53eb9ee8e4ca981107d008f5f9dcc74e5a4
X8Future <151820731+X8Future@users.noreply.github.com>
2025-03-25 18:39:30 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+load_dotenv()
+
+async def load_extensions():
+    await bot.load_extension("cmds.whitelist")
+    await bot.load_extension("cmds.leaderboard")
+    await bot.load_extension("cmds.wlo") 
+    await bot.load_extension("cmds.wc") 
+    await bot.load_extension("cmds.whitelist_delete") 
+    await bot.load_extension("cmds.nord_status")
+    await bot.load_extension("cmds.us_status")
+    await bot.load_extension("cmds.eu_status")
+    await bot.load_extension("cmds.user_remove")
+    await bot.load_extension("cmds.add_run")
+@bot.event
+async def on_ready():
+    await load_extensions()
+    print(f'Logged in as {bot.user}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+async def load_extensions():
+    await bot.load_extension("cmds.whitelist")
+    await bot.load_extension("cmds.leaderboard")
+    await bot.load_extension("cmds.wlo") 
+    await bot.load_extension("cmds.wc") 
+    await bot.load_extension("cmds.whitelist_delete") 
+    await bot.load_extension("cmds.nord_status")
+    await bot.load_extension("cmds.us_status")
+    await bot.load_extension("cmds.eu_status")
+    await bot.load_extension("cmds.hamburg")
+    await bot.load_extension("cmds.add_run")
+    await bot.load_extension("cmds.remove_player")
+    await bot.load_extension("cmds.server_embed")
+
+@bot.event
+async def on_ready():
+    await load_extensions()
+    print(f'Logged in as {bot.user}')
+
+bot.run(os.getenv("DISCORD_TOKEN"))
+
