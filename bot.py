c4180023446d2ca653a5c096a1e12a4b21575d90
X8Future <151820731+X8Future@users.noreply.github.com>
2025-09-18 17:42:13 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,54 +1,45 @@
 import discord
 from discord.ext import commands
-import discord
 import os
-from discord.ext import commands
 from dotenv import load_dotenv
 
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
 load_dotenv()
 
-async def load_extensions():
-    await bot.load_extension("cmds.whitelist")
-    await bot.load_extension("cmds.leaderboard")
-    await bot.load_extension("cmds.wlo")
-    await bot.load_extension("cmds.wc")
-    await bot.load_extension("cmds.whitelist_delete")
-    await bot.load_extension("cmds.add_run")
-    await bot.load_extension("cmds.remove_player")
-    await bot.load_extension("cmds.server_embed")
-    await bot.load_extension("cmds.ban_user")
-    await bot.load_extension("cmds.changetxt")
-    await bot.load_extension("cmds.check_bans")
-@bot.event
-async def on_ready():
-    await load_extensions()
-    print(f'Logged in as {bot.user}')
-
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-async def load_extensions():
-    await bot.load_extension("cmds.whitelist")
-    await bot.load_extension("cmds.leaderboard")
-    await bot.load_extension("cmds.wlo")
-    await bot.load_extension("cmds.wc")
-    await bot.load_extension("cmds.whitelist_delete")
-    await bot.load_extension("cmds.add_run")
-    await bot.load_extension("cmds.remove_player")
-    await bot.load_extension("cmds.server_embed")
-    await bot.load_extension("cmds.ban_user")
-    await bot.load_extension("cmds.changetxt")
-    await bot.load_extension("cmds.check_bans")
+startup_cogs = [
+    "cmds.us_status",
+    "cmds.eu_status",
+    "cmds.hamburg",
+    "cmds.leaderboard",
+    "cmds.live_persons",
+    "cmds.server_embed",
+    "cmds.load_cogs", 
+    "cmds.automod",
+    "cmds.remove_strike"
+]
+
+async def load_startup_cogs():
+    for cog in startup_cogs:
+        try:
+            await bot.load_extension(cog)
+            print(f"✅ Loaded {cog}")
+        except Exception as e:
+            print(f"❌ Failed to load {cog}: {e}")
 
 @bot.event
 async def on_ready():
-    await load_extensions()
     print(f'Logged in as {bot.user}')
+    if not hasattr(bot, 'startup_done'):
+        await load_startup_cogs()
+        bot.startup_done = True
+        try:
+            await bot.tree.sync()
+            print("✅ Slash commands synced.")
+        except Exception as e:
+            print(f"❌ Error syncing slash commands: {e}")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
-
